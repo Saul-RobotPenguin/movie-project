1c8526ca9cd1c8e64802f7ba6d4ac550ef1e3f60
gpalencia <gpalencia@google.com>
2023-08-17 13:48:18 -0400

    hid the api key

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,27 +1,11 @@
 import axios from 'axios'
 
-//   const options = {
-//     method: 'GET',
-//     url: 'https://unogs-unogs-v1.p.rapidapi.com/static/genres',
-//     headers: {
-//       'X-RapidAPI-Key': 'kUcq1Xqq8CAJ8FNm2QzQe8PopG0ChPgx',
-//       'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
-//     }
-//   };
-  
-//   try {
-//       const response = await axios.request(options);
-//       console.log(response.data);
-//   } catch (error) {
-//       console.error(error);
-//   }
-var myHeaders = new Headers();
-myHeaders.append("apikey", "kUcq1Xqq8CAJ8FNm2QzQe8PopG0ChPgx");
-
 let genres = {
   method: 'GET',
   redirect: 'follow',
-  headers: myHeaders
+  headers: {
+    "apikey": `${process.env.REACT_APP_API_KEY}`
+  }
 };
 
 fetch("https://api.apilayer.com/unogs/static/genres", genres)

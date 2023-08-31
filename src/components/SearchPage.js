5a694b69b25b7947744e02d9c21a336c2ad32b5e
gpalencia <gpalencia@google.com>
2023-08-31 13:01:43 -0400

    search api call

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,12 +3,28 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const SearchPage = () => {
+  const [search, setSearch] = useState('')
+  
+  const onChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const Search = async (e) => {
+    e.preventDefault()
+    const { data } = await axios.get('https://api.themoviedb.org/3/search/keyword', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        query: search,
+        page: 1
+      }
+    })
 
 
-
-    return(
-        <div></div>
-    )
+  return(
+    <div></div>
+  )
 }
 
 export default SearchPage

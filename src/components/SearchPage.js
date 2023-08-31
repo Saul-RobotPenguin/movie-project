c25695bf0cdea37fc87f552606b9f36ce937b5bf
gpalencia <gpalencia@google.com>
2023-08-31 13:15:58 -0400

    searchpage component has input form field and displays data

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import {MovieCard} from './MovieCard'
 import axios from 'axios'
 
 const SearchPage = () => {
   const [search, setSearch] = useState('')
+  const [movies, setMovies] = useState('')
   
   const onChange = (e) => {
     setSearch(e.target.value)
@@ -13,17 +15,37 @@ const SearchPage = () => {
     e.preventDefault()
     const { data } = await axios.get('https://api.themoviedb.org/3/search/keyword', {
       headers: {
-        Authorization: `Bearer ${token}`
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
       },
       params: {
         query: search,
         page: 1
       }
     })
+    setMovies(data)
+  }
 
 
   return(
-    <div></div>
+    <div>
+        <div>
+            <form className="searchform" onSubmit={Search}>
+          <input
+            className="searchinput"
+            type="text"
+            name="search"
+            value={search}
+            placeholder="Search"
+            onChange={onChange}
+          >search</input>
+          <button className="searchbutton" type="submit"></button>
+        </form>
+        <div>
+            <MovieCard movies={popularMovies}/>
+        </div>
+        </div>
+    </div>
   )
 }
 
